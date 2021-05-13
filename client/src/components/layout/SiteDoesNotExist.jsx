41ea1ab4b435a4db1f4ca98ea27978100c811c22
Bartosy <drave12345@gmail.com>
2021-05-13 18:16:19 +0200

    deploy

---
+++ client/src/components/layout/SiteDoesNotExist.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const SiteDoesNotExist = ({url}) => {
+
+  return <div>
+      Nope
+  </div>;
+};
+
+export default SiteDoesNotExist;
