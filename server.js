bc12a1d38d943b031ddcfde4c77a6af51921733b
Bartosy <drave12345@gmail.com>
2021-05-13 18:18:34 +0200

    deploy

--- server.js
+++ server.js
@@ -8,22 +8,28 @@ const postsRoutes = require("./routes/api/posts")
 const profileRoutes = require("./routes/api/profile")
 const usersRoutes = require("./routes/api/users")
 const app = express();
-
+const path = require("path")
 //Connect DB
 connectDB();
 
 //Bodyparser
 app.use(express.json({extended: false}))
 
-app.get("/", (req,res)=>{
-    res.send("API is running.")
-})
-
 //Define routes
 app.use("/api/users", usersRoutes)
 app.use("/api/profile", profileRoutes)
 app.use("/api/auth", authRoutes)
 app.use("/api/posts", postsRoutes)
+
+//Serve static assets in production
+if(process.env.NODE_ENV === "production"){
+    //Set static folder
+    app.use(express.static("client/build"))
+
+    app.get("*", (req, res)=>{
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+    })
+}
 
 const PORT = process.env.PORT || 5000;
 
